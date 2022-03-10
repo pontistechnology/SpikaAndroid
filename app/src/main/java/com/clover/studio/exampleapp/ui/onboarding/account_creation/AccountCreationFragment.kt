94d6ff7ae90bea64bfd75f2ebd24f0c65944c056
matkotom <tomljanovic.matko@gmail.com>
2022-03-10 07:45:37 +0100

    Updated chunk size value. #89

--- app/src/main/java/com/clover/studio/exampleapp/ui/onboarding/account_creation/AccountCreationFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/onboarding/account_creation/AccountCreationFragment.kt
@@ -30,7 +30,7 @@ import java.io.FileInputStream
 import java.util.*
 
 
-const val CHUNK_SIZE = 32000
+const val CHUNK_SIZE = 500000
 
 class AccountCreationFragment : BaseFragment() {
     private val viewModel: OnboardingViewModel by activityViewModels()
