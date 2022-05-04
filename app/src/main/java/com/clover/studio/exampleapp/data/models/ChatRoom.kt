f46ad9e2b3e5fc5c514cd388cb433ec3d5967788
matkotom <tomljanovic.matko@gmail.com>
2022-05-04 10:20:39 +0200

    Updated ChatRoom primary key to be autogenerated. #177

--- app/src/main/java/com/clover/studio/exampleapp/data/models/ChatRoom.kt
+++ app/src/main/java/com/clover/studio/exampleapp/data/models/ChatRoom.kt
@@ -3,13 +3,14 @@ package com.clover.studio.exampleapp.data.models
 import androidx.room.*
 import com.clover.studio.exampleapp.data.AppDatabase
 import com.clover.studio.exampleapp.data.models.networking.RoomUsers
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = AppDatabase.TablesInfo.TABLE_CHAT_ROOM)
 data class ChatRoom(
 
-    @PrimaryKey
-    @ColumnInfo(name = AppDatabase.TablesInfo.ID)
-    val id: Int,
+    @SerializedName("id")
+    @ColumnInfo(name = "room_id")
+    val roomId: Int,
 
     @ColumnInfo(name = "name")
     val name: String?,
@@ -26,4 +27,9 @@ data class ChatRoom(
 
     @ColumnInfo(name = "created_at")
     val createdAt: Long?
-)
+) {
+    @Transient
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = AppDatabase.TablesInfo.ID)
+    var id: Int = 0
+}
