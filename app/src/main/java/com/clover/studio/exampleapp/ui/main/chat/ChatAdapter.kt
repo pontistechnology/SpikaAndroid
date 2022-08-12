250e9008ce2abacb33dc0542d984702651c3c6eb
matkotom <tomljanovic.matko@gmail.com>
2022-08-12 14:28:35 +0200

    Added option to display files from other users.

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat/ChatAdapter.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat/ChatAdapter.kt
@@ -69,6 +69,9 @@ class ChatAdapter(
             calendar.timeInMillis = it.createdAt!!
             val date = calendar.get(Calendar.DAY_OF_MONTH)
 
+            // View holder for my messages
+
+            // TODO can two view holders use same method for binding if all views are the same?
             if (holder.itemViewType == VIEW_TYPE_MESSAGE_SENT) {
                 holder as SentMessageHolder
                 when (it.type) {
@@ -161,7 +164,60 @@ class ChatAdapter(
                     }
                 }
             } else {
-                (holder as ReceivedMessageHolder).binding.tvMessage.text = it.body?.text
+                // View holder for messages from other users
+                holder as ReceivedMessageHolder
+                when (it.type) {
+                    Const.JsonFields.TEXT -> {
+                        holder.binding.tvMessage.text = it.body?.text
+                        holder.binding.tvMessage.visibility = View.VISIBLE
+                        holder.binding.ivChatImage.visibility = View.GONE
+                        holder.binding.clFileMessage.visibility = View.GONE
+                    }
+                    Const.JsonFields.CHAT_IMAGE -> {
+                        holder.binding.tvMessage.visibility = View.GONE
+                        holder.binding.ivChatImage.visibility = View.VISIBLE
+                        holder.binding.clFileMessage.visibility = View.GONE
+
+                        Glide.with(context)
+                            .load(it.body?.file?.path?.let { imagePath ->
+                                Tools.getAvatarUrl(
+                                    imagePath
+                                )
+                            })
+                            .into(holder.binding.ivChatImage)
+                    }
+                    Const.JsonFields.FILE_TYPE -> {
+                        holder.binding.tvMessage.visibility = View.GONE
+                        holder.binding.ivChatImage.visibility = View.GONE
+                        holder.binding.clFileMessage.visibility = View.VISIBLE
+
+                        holder.binding.tvFileTitle.text = it.body?.file?.fileName
+                        holder.binding.tvFileSize.text = it.body?.file?.size.toString()
+
+                        when (it.body?.file?.fileName?.substringAfterLast(".")) {
+                            Const.FileExtensions.PDF -> holder.binding.ivFileType.setImageDrawable(
+                                context.resources.getDrawable(
+                                    R.drawable.img_pdf, null
+                                )
+                            )
+                            Const.FileExtensions.ZIP -> holder.binding.ivFileType.setImageDrawable(
+                                context.resources.getDrawable(
+                                    R.drawable.img_zip, null
+                                )
+                            )
+                            else -> holder.binding.ivFileType.setImageDrawable(
+                                context.resources.getDrawable(
+                                    R.drawable.img_word, null
+                                )
+                            )
+                        }
+                    }
+                    else -> {
+                        holder.binding.tvMessage.visibility = View.VISIBLE
+                        holder.binding.ivChatImage.visibility = View.GONE
+                        holder.binding.clFileMessage.visibility = View.GONE
+                    }
+                }
 
                 if (it.body?.text.isNullOrEmpty()) {
                     holder.binding.tvMessage.visibility = View.GONE
