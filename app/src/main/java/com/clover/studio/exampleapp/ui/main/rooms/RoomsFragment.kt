7a433f3d94182dc599580306b31f0b62663f9fad
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2022-09-23 09:09:25 +0200

    RoomsFragment - returned notifyDataSetChanged to update the elapsed time

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/rooms/RoomsFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/rooms/RoomsFragment.kt
@@ -162,6 +162,9 @@ class RoomsFragment : BaseFragment() {
     override fun onResume() {
         super.onResume()
         viewModel.getRooms()
+
         // This updates the elapsed time displayed when user return to the screen.
+        // TODO: it needs to be removed at one point
+        roomsAdapter.notifyDataSetChanged()
     }
 }
