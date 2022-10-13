f7417a937b905d69388bfcb9f04562f85b58c269
MatkoTom <matko@clover.studio>
2022-10-13 10:36:01 +0200

    Added documentation

--- app/src/main/java/com/clover/studio/exampleapp/utils/helpers/AppLifecycleManager.kt
+++ app/src/main/java/com/clover/studio/exampleapp/utils/helpers/AppLifecycleManager.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 
+// Singleton class which observes application lifecycle events and notifies caller if the app
+// is in foreground or background
 object AppLifecycleManager : LifecycleEventObserver {
     var isInForeground = true
 
