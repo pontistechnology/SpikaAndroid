7b324aff8cfa90917e7345b08acd27d3582b47c6
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2022-10-23 14:51:04 +0200

    implementation of reactions and number of reactions in group chats, fixed: reacting to images extracted some values

---
+++ app/src/main/java/com/clover/studio/exampleapp/data/models/Reactions.kt
@@ -0,0 +1,10 @@
+package com.clover.studio.exampleapp.data.models
+
+data class Reactions(
+    var thumbsUp : Int,
+    var heart : Int,
+    var prayingHandsEmoji: Int,
+    var astonishedEmoji: Int,
+    var relievedEmoji: Int,
+    var cryingFaceEmoji: Int,
+)
