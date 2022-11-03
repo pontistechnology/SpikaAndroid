4a3b4e85a3cce88bcd43635896cdb7134da6a0fd
MatkoTom <matko@clover.studio>
2022-11-03 13:34:24 +0100

    Removed unused methods from services.

--- app/src/main/java/com/clover/studio/exampleapp/data/services/ChatService.kt
+++ app/src/main/java/com/clover/studio/exampleapp/data/services/ChatService.kt
@@ -1,6 +1,5 @@
 package com.clover.studio.exampleapp.data.services
 
-import com.clover.studio.exampleapp.data.models.networking.MessageRecordsResponse
 import com.clover.studio.exampleapp.data.models.networking.MessageResponse
 import com.clover.studio.exampleapp.data.models.networking.RoomResponse
 import com.clover.studio.exampleapp.data.models.networking.SettingsResponse
@@ -14,30 +13,6 @@ interface ChatService {
         @HeaderMap headers: Map<String, String?>,
         @Body jsonObject: JsonObject
     ): MessageResponse
-
-    @GET(Const.Networking.API_GET_MESSAGES)
-    suspend fun getMessages(
-        @HeaderMap headers: Map<String, String?>,
-        @Path(Const.Networking.ROOM_ID) roomId: String
-    ): MessageResponse
-
-    @GET(Const.Networking.API_GET_MESSAGES_TIMESTAMP)
-    suspend fun getMessagesTimestamp(
-        @HeaderMap headers: Map<String, String?>,
-        @Query(Const.Networking.TIMESTAMP) timestamp: Int
-    ): MessageResponse
-
-    @POST(Const.Networking.API_MESSAGE_DELIVERED)
-    suspend fun sendMessageDelivered(
-        @HeaderMap headers: Map<String, String?>,
-        @Body jsonObject: JsonObject
-    ): MessageRecordsResponse
-
-//    @GET(Const.Networking.API_GET_MESSAGE_RECORDS)
-//    suspend fun getMessageRecords(
-//        @HeaderMap headers: Map<String, String?>,
-//        @Path(Const.Networking.MESSAGE_ID) messageId: String
-//    ): MessageRecordsResponse
 
     @POST(Const.Networking.API_MESSAGES_SEEN)
     suspend fun sendMessagesSeen(
--- app/src/main/java/com/clover/studio/exampleapp/data/services/RetrofitService.kt
+++ app/src/main/java/com/clover/studio/exampleapp/data/services/RetrofitService.kt
@@ -1,21 +1,11 @@
 package com.clover.studio.exampleapp.data.services
 
-import com.clover.studio.exampleapp.data.models.networking.ContactResponse
-import com.clover.studio.exampleapp.data.models.networking.MessageRecordsResponse
-import com.clover.studio.exampleapp.data.models.networking.MessageResponse
-import com.clover.studio.exampleapp.data.models.networking.RoomResponse
 import com.clover.studio.exampleapp.data.models.networking.*
 import com.clover.studio.exampleapp.utils.Const
 import com.google.gson.JsonObject
 import retrofit2.http.*
 
 interface RetrofitService {
-    @GET(Const.Networking.API_CONTACTS)
-    suspend fun getUsers(
-        @HeaderMap headers: Map<String, String?>,
-        @Query(Const.Networking.PAGE) page: Int
-    ): ContactResponse
-
     @GET(Const.Networking.API_GET_ROOM_BY_ID)
     suspend fun getRoomById(
         @HeaderMap headers: Map<String, String?>,
@@ -27,24 +17,6 @@ interface RetrofitService {
         @HeaderMap headers: Map<String, String?>,
         @Body jsonObject: JsonObject
     ): RoomResponse
-
-    @GET(Const.Networking.API_POST_NEW_ROOM)
-    suspend fun getRooms(
-        @HeaderMap headers: Map<String, String?>,
-        @Query(Const.Networking.PAGE) page: Int
-    ): RoomResponse
-
-    @GET(Const.Networking.API_GET_MESSAGES)
-    suspend fun getMessages(
-        @HeaderMap headers: Map<String, String?>,
-        @Path(Const.Networking.ROOM_ID) roomId: String
-    ): MessageResponse
-
-    @GET(Const.Networking.API_GET_MESSAGE_RECORDS)
-    suspend fun getMessageRecords(
-        @HeaderMap headers: Map<String, String?>,
-        @Path(Const.Networking.MESSAGE_ID) messageId: String
-    ): MessageRecordsResponse
 
     @PUT(Const.Networking.API_UPDATE_TOKEN)
     suspend fun updatePushToken(
