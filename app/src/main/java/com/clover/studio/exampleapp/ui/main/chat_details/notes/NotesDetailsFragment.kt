b264648d6d4195bdb5eb1c0cde3200c01ebd3838
MatkoTom <matko@clover.studio>
2023-01-03 15:13:52 +0100

    Added Markwon library for markdown strings and implemented it in NoteDetailsFragment.

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesDetailsFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesDetailsFragment.kt
@@ -1,12 +1,21 @@
 package com.clover.studio.exampleapp.ui.main.chat_details.notes
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.util.Linkify
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.clover.studio.exampleapp.databinding.FragmentNotesDetailsBinding
+import io.noties.markwon.LinkResolver
+import io.noties.markwon.Markwon
+import org.commonmark.node.Link
 
 class NotesDetailsFragment : Fragment() {
     private var bindingSetup: FragmentNotesDetailsBinding? = null
@@ -26,11 +35,15 @@ class NotesDetailsFragment : Fragment() {
         bindingSetup = FragmentNotesDetailsBinding.inflate(inflater, container, false)
 
         initializeViews()
+
         // Inflate the layout for this fragment
         return binding.root
     }
 
     private fun initializeViews() {
-        binding.tvNotesDetails.text = notes
+        val markwon = Markwon.create(requireContext())
+
+        markwon.setMarkdown(binding.tvNotesDetails, notes)
+        Linkify.addLinks(binding.tvNotesDetails, Linkify.WEB_URLS)
     }
 }
