c392d269f5f704e696f887ddf354af862185b9c4
MatkoTom <matko@clover.studio>
2023-01-03 13:55:33 +0100

    Rebase #3

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
