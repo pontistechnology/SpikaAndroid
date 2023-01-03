5458249d3fab93b26781d39ab1ac662f2fbce868
MatkoTom <matko@clover.studio>
2023-01-03 12:59:53 +0100

    Rebase #1

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesFragment.kt
@@ -1,16 +1,20 @@
 package com.clover.studio.exampleapp.ui.main.chat_details.notes
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.clover.studio.exampleapp.R
+import com.clover.studio.exampleapp.databinding.FragmentNotesBinding
+import com.clover.studio.exampleapp.ui.main.chat.ChatViewModel
+import com.clover.studio.exampleapp.ui.main.chat.NotesFetched
+import com.clover.studio.exampleapp.utils.EventObserver
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.clover.studio.exampleapp.databinding.FragmentNotesBinding
-import com.clover.studio.exampleapp.ui.main.chat.ChatViewModel
 import com.clover.studio.exampleapp.utils.extendables.BaseFragment
 
 class NotesFragment : BaseFragment() {
@@ -24,7 +28,6 @@ class NotesFragment : BaseFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         roomId = args.roomId
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
