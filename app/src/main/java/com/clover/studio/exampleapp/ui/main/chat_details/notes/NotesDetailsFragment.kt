cfcf3c5e03b870d63bd4cc23b293e3afd8dc4f41
MatkoTom <matko@clover.studio>
2023-01-05 14:05:43 +0100

    Rebase #6

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesDetailsFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat_details/notes/NotesDetailsFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.clover.studio.exampleapp.R
 import com.clover.studio.exampleapp.databinding.FragmentNotesDetailsBinding
+import com.clover.studio.exampleapp.ui.main.chat.ChatViewModel
 import io.noties.markwon.LinkResolver
 import org.commonmark.node.Link
 import io.noties.markwon.Markwon
@@ -15,6 +18,7 @@ import io.noties.markwon.Markwon
 class NotesDetailsFragment : Fragment() {
     private var bindingSetup: FragmentNotesDetailsBinding? = null
     private val binding get() = bindingSetup!!
+    private val viewModel: ChatViewModel by activityViewModels()
     private val args: NotesDetailsFragmentArgs by navArgs()
     private var notes: String = ""
     private var notesName: String = ""
@@ -47,6 +51,29 @@ class NotesDetailsFragment : Fragment() {
         binding.ivBack.setOnClickListener {
             activity?.onBackPressed()
         }
+
         binding.tvNotesDetails.text = notes
+
+        binding.tvEdit.setOnClickListener {
+            if (binding.tvEdit.text == getString(R.string.save)) {
+                // TODO upload updated note
+                binding.tvTitle.visibility = View.VISIBLE
+                binding.tvNotesDetails.visibility = View.VISIBLE
+                binding.etTitle.visibility = View.GONE
+                binding.etDescription.visibility = View.GONE
+
+                binding.tvEdit.text = getString(R.string.edit)
+            } else {
+                binding.tvTitle.visibility = View.GONE
+                binding.tvNotesDetails.visibility = View.GONE
+                binding.etTitle.visibility = View.VISIBLE
+                binding.etDescription.visibility = View.VISIBLE
+
+                binding.tvEdit.text = getString(R.string.save)
+
+                binding.etTitle.setText(notesName)
+                binding.etDescription.setText(notes)
+            }
+        }
     }
 }
