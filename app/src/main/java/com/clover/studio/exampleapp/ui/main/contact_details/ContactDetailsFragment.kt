9354eaf320866a4d476ab8ec90c898515e9d8085
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-01-19 10:34:00 +0100

    Removed error dialog when screen was entered and user has no avatar uploaded. #635 (#105)
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/contact_details/ContactDetailsFragment.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/contact_details/ContactDetailsFragment.kt
@@ -1,16 +1,12 @@
 package com.clover.studio.exampleapp.ui.main.contact_details
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.clover.studio.exampleapp.R
 import com.clover.studio.exampleapp.data.models.entity.User
 import com.clover.studio.exampleapp.databinding.FragmentContactDetailsBinding
@@ -166,45 +162,15 @@ class ContactDetailsFragment : BaseFragment() {
             }
 
             Glide.with(this).load(user?.avatarFileId?.let { getFilePathUrl(it) })
-                .listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        Timber.d("Failed to load user image")
-                        DialogError.getInstance(requireActivity(),
-                            getString(R.string.error),
-                            getString(R.string.failed_user_image),
-                            null,
-                            getString(R.string.ok),
-                            object : DialogInteraction {
-                                override fun onFirstOptionClicked() {
-                                    // ignore
-                                }
-
-                                override fun onSecondOptionClicked() {
-                                    // ignore
-                                }
-                            })
-                        binding.clProgressScreen.visibility = View.GONE
-                        return false
-                    }
-
-                    override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        Timber.d("Image loaded successfully")
-                        binding.clProgressScreen.visibility = View.GONE
-                        return false
-                    }
-                })
+                .placeholder(
+                    ResourcesCompat.getDrawable(
+                        requireContext().resources,
+                        R.drawable.img_user_placeholder,
+                        null
+                    )
+                )
                 .into(binding.ivPickAvatar)
+            binding.clProgressScreen.visibility = View.GONE
         }
 
         binding.ivBack.setOnClickListener {
