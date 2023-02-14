90159b59b1d91ec4a96d4e11e4d1d994b70160fc
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-02-14 14:24:12 +0100

    Video mp4 Convert (#135)
    * Implemented functionality for sending video files as mp4 files. #782
    
    * Small name fix
    
    * Grammar mistake fix
    
    ---------
    
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/exampleapp/utils/Tools.kt
+++ app/src/main/java/com/clover/studio/exampleapp/utils/Tools.kt
@@ -5,6 +5,7 @@ import android.app.DownloadManager
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.*
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
@@ -12,6 +13,7 @@ import android.telephony.PhoneNumberUtils
 import android.telephony.TelephonyManager
 import android.text.TextUtils
 import android.text.format.DateUtils
+import android.util.Log
 import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
@@ -22,6 +24,7 @@ import androidx.core.net.toUri
 import androidx.exifinterface.media.ExifInterface
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils.rotateImage
 import com.clover.studio.exampleapp.BuildConfig
+import com.clover.studio.exampleapp.MainApplication
 import com.clover.studio.exampleapp.R
 import com.clover.studio.exampleapp.data.models.entity.Message
 import com.clover.studio.exampleapp.data.models.entity.MessageBody
@@ -29,6 +32,7 @@ import retrofit2.HttpException
 import timber.log.Timber
 import java.io.*
 import java.math.BigInteger
+import java.nio.ByteBuffer
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.text.DecimalFormat
@@ -411,5 +415,108 @@ object Tools {
     fun fullDateFormat(dateTime: Long): String? {
         val simpleDateFormat = SimpleDateFormat("dd.MM.yyyy. HH:mm aa", Locale.getDefault())
         return simpleDateFormat.format(dateTime)
+    }
+
+    /**
+     * Code generates video file in mp4 format by decoding it piece by piece
+     *
+     * @param srcUri Source URI to be converted to mp4 format
+     * @param dstPath Destination path for the converted video file
+     * @param startMs Can be used for start time when trimming a video
+     * @param endMs Can be used for end time when trimming video
+     * @param useAudio Boolean which decides if new file will have audio
+     * @param useVideo Boolean which decides if new file will have video
+     */
+    @Throws(IOException::class)
+    fun genVideoUsingMuxer(
+        srcUri: Uri?,
+        dstPath: String?,
+        startMs: Long = 0L,
+        endMs: Long = 0L,
+        useAudio: Boolean = true,
+        useVideo: Boolean = true
+    ) {
+        // Set up MediaExtractor to read from the source.
+        val extractor = MediaExtractor()
+        extractor.setDataSource(MainApplication.appContext, srcUri!!, null)
+        val trackCount = extractor.trackCount
+        // Set up MediaMuxer for the destination.
+        val muxer = MediaMuxer(dstPath!!, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+        // Set up the tracks and retrieve the max buffer size for selected
+        // tracks.
+        val indexMap: HashMap<Int, Int> = HashMap(trackCount)
+        var bufferSize = -1
+        for (i in 0 until trackCount) {
+            val format = extractor.getTrackFormat(i)
+            val mime = format.getString(MediaFormat.KEY_MIME)
+            var selectCurrentTrack = false
+            if (mime!!.startsWith("audio/") && useAudio) {
+                selectCurrentTrack = true
+            } else if (mime.startsWith("video/") && useVideo) {
+                selectCurrentTrack = true
+            }
+            if (selectCurrentTrack) {
+                extractor.selectTrack(i)
+                val dstIndex = muxer.addTrack(format)
+                indexMap[i] = dstIndex
+                if (format.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
+                    val newSize = format.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
+                    bufferSize = newSize.coerceAtLeast(bufferSize)
+                }
+            }
+        }
+        if (bufferSize < 0) {
+            bufferSize = DEFAULT_BUFFER_SIZE
+        }
+        // Set up the orientation and starting time for extractor.
+        val retrieverSrc = MediaMetadataRetriever()
+        retrieverSrc.setDataSource(MainApplication.appContext, srcUri)
+        val degreesString = retrieverSrc.extractMetadata(
+            MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION
+        )
+        if (degreesString != null) {
+            val degrees = degreesString.toInt()
+            if (degrees >= 0) {
+                muxer.setOrientationHint(degrees)
+            }
+        }
+
+        if (startMs > 0) {
+            extractor.seekTo(startMs * 1000L, MediaExtractor.SEEK_TO_CLOSEST_SYNC)
+        }
+        // Copy the samples from MediaExtractor to MediaMuxer. We will loop
+        // for copying each sample and stop when we get to the end of the source
+        // file or exceed the end time of the trimming.
+        val offset = 0
+        var trackIndex: Int
+        val dstBuf: ByteBuffer = ByteBuffer.allocate(bufferSize)
+        val bufferInfo = MediaCodec.BufferInfo()
+        muxer.start()
+        while (true) {
+            bufferInfo.offset = offset
+            bufferInfo.size = extractor.readSampleData(dstBuf, offset)
+            if (bufferInfo.size < 0) {
+                Log.d("LOGTAG", "Saw input EOS.")
+                bufferInfo.size = 0
+                break
+            } else {
+                bufferInfo.presentationTimeUs = extractor.sampleTime
+                // Code used when trimming videos
+                if (endMs > 0 && bufferInfo.presentationTimeUs > endMs * 1000L) {
+                    Log.d("LOGTAG", "The current sample is over the trim end time.")
+                    break
+                } else {
+                    bufferInfo.flags = extractor.sampleFlags
+                    trackIndex = extractor.sampleTrackIndex
+                    muxer.writeSampleData(
+                        indexMap[trackIndex]!!, dstBuf,
+                        bufferInfo
+                    )
+                    extractor.advance()
+                }
+            }
+        }
+        muxer.stop()
+        muxer.release()
     }
 }
