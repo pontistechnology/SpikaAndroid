31d0aba3d71d8c891c6949026b86fc24d58458fe
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-04-26 10:49:25 +0200

    versionCode++ 100019 (#200)
    Updated migration for version 1 -> 2
    
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/exampleapp/data/AppDatabase.kt
+++ app/src/main/java/com/clover/studio/exampleapp/data/AppDatabase.kt
@@ -78,7 +78,7 @@ abstract class AppDatabase : RoomDatabase() {
         val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE " + TablesInfo.TABLE_MESSAGE_RECORDS + " ADD COLUMN record_message TEXT")
-                database.execSQL("ALTER TABLE " + TablesInfo.TABLE_CHAT_ROOM + " ADD COLUMN unread_count INTEGER DEFAULT 0 NOT NULL")
+                database.execSQL("ALTER TABLE " + TablesInfo.TABLE_USER + " ADD COLUMN is_bot INTEGER NOT NULL DEFAULT 0")
             }
         }
         /* Database migration from version 2 to 3.
