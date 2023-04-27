f16405cfdfa0eea22f8c160d6e8e52504136bbca
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-04-27 11:43:23 +0200

    Migration Test (#208)
    * Migration test work, also fixed debug build from not running
    
    * Added schemas
    
    * Finished work on migration test functionality.
    
    ---------
    
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/androidTest/java/com/clover/studio/exampleapp/MigrationTest.kt
+++ app/src/androidTest/java/com/clover/studio/exampleapp/MigrationTest.kt
@@ -9,6 +9,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.clover.studio.exampleapp.data.AppDatabase
 import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import timber.log.Timber
 import java.io.IOException
@@ -16,6 +17,7 @@ import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class MigrationTest {
+    @JvmField
     @Rule
     var helper: MigrationTestHelper = MigrationTestHelper(
         InstrumentationRegistry.getInstrumentation(), Objects.requireNonNull(
@@ -27,20 +29,29 @@ class MigrationTest {
      * Implement below migration test when testing actual migrations. Change function and adapt to
      * your needs
      */
+    @Test
+    @Throws(IOException::class)
+    fun migrate1To2() {
+        val currentVersion = 1
+        val newVersion = 2
+        testMigration(currentVersion, newVersion, AppDatabase.MIGRATION_1_2)
+    }
+
+    /** Migration 2 to 3 test, when needed */
 //    @Test
 //    @Throws(IOException::class)
-//    fun migrate1To2() {
-//        val currentVersion = 1
-//        val newVersion = 2
-//        testMigration(currentVersion, newVersion, AppDatabase.MIGRATION_1_2)
+//    fun migrate2To3() {
+//        val currentVersion = 2
+//        val newVersion = 3
+//        testMigration(currentVersion, newVersion, AppDatabase.MIGRATION_2_3)
 //    }
 
     @Throws(IOException::class)
     private fun testMigration(currentVersion: Int, newVersion: Int, migration: Migration) {
         var db: SupportSQLiteDatabase = helper.createDatabase(TEST_DB, currentVersion)
         db.execSQL(
-            "INSERT INTO " + AppDatabase.TablesInfo.TABLE_CHAT_ROOM + " (id, description, email, name) VALUES ('" + System.currentTimeMillis()
-                .toString() + "', 'TestDescription', 'TestEmail', 'TestName')"
+            "INSERT INTO " + AppDatabase.TablesInfo.TABLE_CHAT_ROOM + " (created_at, name, type, muted, pinned, unread_count) VALUES ('" + System.currentTimeMillis()
+                .toString() + "', 'TestName', 'TestType', '1', '2', '3')"
         )
         db.close()
         db = helper.runMigrationsAndValidate(TEST_DB, newVersion, true, migration)
