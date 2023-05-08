72e8fc2f17fa924c1b3d11f3225174834fd44b3c
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-05-08 14:27:37 +0200

    Setup migration insert logic for data that needs to persist over database versions. (#224)
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/exampleapp/data/AppDatabase.kt
+++ app/src/main/java/com/clover/studio/exampleapp/data/AppDatabase.kt
@@ -87,7 +87,9 @@ abstract class AppDatabase : RoomDatabase() {
 
         val MIGRATION_2_3: Migration = object : Migration(2, 3) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("CREATE TABLE IF NOT EXISTS " + TablesInfo.TABLE_CHAT_ROOM_NEW + " (`room_id` INTEGER NOT NULL, `name` TEXT, `type` TEXT, `avatar_file_id` INTEGER, `created_at` INTEGER, `modified_at` INTEGER, `muted` INTEGER NOT NULL, `pinned` INTEGER NOT NULL, `room_exit` INTEGER, `unread_count` INTEGER NOT NULL, PRIMARY KEY(`room_id`))")
+                database.execSQL("ALTER TABLE " + TablesInfo.TABLE_CHAT_ROOM + " ADD COLUMN deleted INTEGER NOT NULL DEFAULT 0")
+                database.execSQL("CREATE TABLE IF NOT EXISTS " + TablesInfo.TABLE_CHAT_ROOM_NEW + " (`room_id` INTEGER NOT NULL, `name` TEXT, `type` TEXT, `avatar_file_id` INTEGER, `created_at` INTEGER, `modified_at` INTEGER, `muted` INTEGER NOT NULL, `pinned` INTEGER NOT NULL, `room_exit` INTEGER,  `deleted` INTEGER NOT NULL, `unread_count` INTEGER NOT NULL, PRIMARY KEY(`room_id`))")
+                database.execSQL("INSERT INTO " + TablesInfo.TABLE_CHAT_ROOM_NEW + " (room_id, name, type, avatar_file_id, created_at, modified_at, muted, pinned, room_exit, deleted, unread_count) SELECT room_id, name, type, avatar_file_id, created_at, modified_at, muted, pinned, room_exit, deleted, unread_count FROM room")
                 database.execSQL("DROP TABLE " + TablesInfo.TABLE_CHAT_ROOM)
                 database.execSQL("ALTER TABLE " + TablesInfo.TABLE_CHAT_ROOM_NEW + " RENAME TO " + TablesInfo.TABLE_CHAT_ROOM)
 
@@ -95,8 +97,6 @@ abstract class AppDatabase : RoomDatabase() {
                 database.execSQL("ALTER TABLE " + TablesInfo.TABLE_MESSAGE + " RENAME COLUMN room_id TO room_id_message")
                 database.execSQL("ALTER TABLE " + TablesInfo.TABLE_MESSAGE + " RENAME COLUMN created_at TO created_at_message")
                 database.execSQL("ALTER TABLE " + TablesInfo.TABLE_MESSAGE + " RENAME COLUMN modified_at TO modified_at_message")
-
-                database.execSQL("ALTER TABLE " + TablesInfo.TABLE_CHAT_ROOM + " ADD COLUMN deleted INTEGER NOT NULL DEFAULT 0")
             }
         }
 
