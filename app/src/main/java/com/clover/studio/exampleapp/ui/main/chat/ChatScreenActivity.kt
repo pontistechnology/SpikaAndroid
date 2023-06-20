1c580c91c3fea4a149e0c2439a4fbe84020ed147
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2023-06-20 13:13:00 +0200

    Room Notification Removal (#246)
    * Removed room notification from the app until we decide how to implement it correctly
    
    * Added some comments
    
    ---------
    
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/exampleapp/ui/main/chat/ChatScreenActivity.kt
+++ app/src/main/java/com/clover/studio/exampleapp/ui/main/chat/ChatScreenActivity.kt
@@ -1,24 +1,18 @@
 package com.clover.studio.exampleapp.ui.main.chat
 
-import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
-import com.bumptech.glide.Glide
 import com.clover.studio.exampleapp.R
 import com.clover.studio.exampleapp.data.models.junction.RoomWithUsers
 import com.clover.studio.exampleapp.databinding.ActivityChatScreenBinding
 import com.clover.studio.exampleapp.ui.onboarding.startOnboardingActivity
 import com.clover.studio.exampleapp.utils.Const
 import com.clover.studio.exampleapp.utils.EventObserver
-import com.clover.studio.exampleapp.utils.Tools
 import com.clover.studio.exampleapp.utils.UploadDownloadManager
 import com.clover.studio.exampleapp.utils.dialog.DialogError
 import com.clover.studio.exampleapp.utils.extendables.BaseActivity
@@ -50,11 +44,13 @@ class ChatScreenActivity : BaseActivity() {
 
     private lateinit var bindingSetup: ActivityChatScreenBinding
     private val viewModel: ChatViewModel by viewModels()
-    private var handler = Handler(Looper.getMainLooper())
-    private var runnable: Runnable = Runnable {
-        Timber.d("Ending handler")
-        bindingSetup.cvNotification.cvRoot.visibility = View.GONE
-    }
+
+    /** These two fields are used for the room notification, which has been removed temporarily **/
+//    private var handler = Handler(Looper.getMainLooper())
+//    private var runnable: Runnable = Runnable {
+//        Timber.d("Ending handler")
+//        bindingSetup.cvNotification.cvRoot.visibility = View.GONE
+//    }
 
     @Inject
     lateinit var uploadDownloadManager: UploadDownloadManager
@@ -101,105 +97,106 @@ class ChatScreenActivity : BaseActivity() {
             }
         })
 
-        viewModel.roomNotificationListener.observe(this, EventObserver {
-            when (it.response.status) {
-                Resource.Status.SUCCESS -> {
-                    val myUserId = viewModel.getLocalUserId()
-
-                    if (myUserId == it.message.fromUserId || roomWithUsers?.room?.roomId == it.message.roomId || it.response.responseData?.room!!.muted) return@EventObserver
-                    runOnUiThread {
-                        val animator =
-                            ValueAnimator.ofInt(
-                                bindingSetup.cvNotification.pbTimeout.max,
-                                0
-                            )
-                        animator.duration = 5000
-                        animator.addUpdateListener { animation ->
-                            bindingSetup.cvNotification.pbTimeout.progress =
-                                animation.animatedValue as Int
-                        }
-                        animator.start()
-
-                        if (it.response.responseData.room.type.equals(Const.JsonFields.GROUP)) {
-                            Glide.with(this@ChatScreenActivity)
-                                .load(it.response.responseData.room.avatarFileId?.let { fileId ->
-                                    Tools.getFilePathUrl(
-                                        fileId
-                                    )
-                                })
-                                .placeholder(R.drawable.img_user_placeholder)
-                                .centerCrop()
-                                .into(bindingSetup.cvNotification.ivUserImage)
-                            bindingSetup.cvNotification.tvTitle.text =
-                                it.response.responseData.room.name
-                            for (user in it.response.responseData.users) {
-                                if (user.id != myUserId && user.id == it.message.fromUserId) {
-                                    val content: String =
-                                        if (it.message.type != Const.JsonFields.TEXT_TYPE) {
-                                            user.formattedDisplayName + ": " + getString(
-                                                R.string.generic_shared,
-                                                it.message.type.toString()
-                                                    .replaceFirstChar { type -> type.uppercase() })
-                                        } else {
-                                            user.formattedDisplayName + ": " + it.message.body?.text.toString()
-                                        }
-
-                                    bindingSetup.cvNotification.tvMessage.text =
-                                        content
-                                    break
-                                }
-                            }
-                        } else {
-                            for (user in it.response.responseData.users) {
-                                if (user.id != myUserId && user.id == it.message.fromUserId) {
-                                    Glide.with(this@ChatScreenActivity)
-                                        .load(user.avatarFileId?.let { fileId ->
-                                            Tools.getFilePathUrl(
-                                                fileId
-                                            )
-                                        })
-                                        .placeholder(R.drawable.img_user_placeholder)
-                                        .centerCrop()
-                                        .into(bindingSetup.cvNotification.ivUserImage)
-                                    val content: String =
-                                        if (it.message.type != Const.JsonFields.TEXT_TYPE) {
-                                            getString(
-                                                R.string.generic_shared,
-                                                it.message.type.toString()
-                                                    .replaceFirstChar { type -> type.uppercase() })
-                                        } else {
-                                            it.message.body?.text.toString()
-                                        }
-
-                                    bindingSetup.cvNotification.tvTitle.text =
-                                        user.formattedDisplayName
-                                    bindingSetup.cvNotification.tvMessage.text =
-                                        content
-                                    break
-                                }
-                            }
-                        }
-
-
-                        bindingSetup.cvNotification.cvRoot.visibility = View.VISIBLE
-
-                        val roomId = it.message.roomId
-                        bindingSetup.cvNotification.cvRoot.setOnClickListener {
-                            roomId?.let { roomId -> viewModel.getSingleRoomData(roomId) }
-                        }
-
-                        runnable.let { runnable -> handler.removeCallbacks(runnable) }
-
-                        handler = Handler(Looper.getMainLooper())
-                        Timber.d("Starting handler")
-                        handler.postDelayed(runnable, 5000)
-                    }
-                }
-
-                Resource.Status.ERROR -> Timber.d("Failed to fetch room with users")
-                else -> Timber.d("Other error")
-            }
-        })
+        /** Room notification has been disabled until we decide how to implement it correctly **/
+//        viewModel.roomNotificationListener.observe(this, EventObserver {
+//            when (it.response.status) {
+//                Resource.Status.SUCCESS -> {
+//                    val myUserId = viewModel.getLocalUserId()
+//
+//                    if (myUserId == it.message.fromUserId || roomWithUsers?.room?.roomId == it.message.roomId || it.response.responseData?.room!!.muted) return@EventObserver
+//                    runOnUiThread {
+//                        val animator =
+//                            ValueAnimator.ofInt(
+//                                bindingSetup.cvNotification.pbTimeout.max,
+//                                0
+//                            )
+//                        animator.duration = 5000
+//                        animator.addUpdateListener { animation ->
+//                            bindingSetup.cvNotification.pbTimeout.progress =
+//                                animation.animatedValue as Int
+//                        }
+//                        animator.start()
+//
+//                        if (it.response.responseData.room.type.equals(Const.JsonFields.GROUP)) {
+//                            Glide.with(this@ChatScreenActivity)
+//                                .load(it.response.responseData.room.avatarFileId?.let { fileId ->
+//                                    Tools.getFilePathUrl(
+//                                        fileId
+//                                    )
+//                                })
+//                                .placeholder(R.drawable.img_user_placeholder)
+//                                .centerCrop()
+//                                .into(bindingSetup.cvNotification.ivUserImage)
+//                            bindingSetup.cvNotification.tvTitle.text =
+//                                it.response.responseData.room.name
+//                            for (user in it.response.responseData.users) {
+//                                if (user.id != myUserId && user.id == it.message.fromUserId) {
+//                                    val content: String =
+//                                        if (it.message.type != Const.JsonFields.TEXT_TYPE) {
+//                                            user.formattedDisplayName + ": " + getString(
+//                                                R.string.generic_shared,
+//                                                it.message.type.toString()
+//                                                    .replaceFirstChar { type -> type.uppercase() })
+//                                        } else {
+//                                            user.formattedDisplayName + ": " + it.message.body?.text.toString()
+//                                        }
+//
+//                                    bindingSetup.cvNotification.tvMessage.text =
+//                                        content
+//                                    break
+//                                }
+//                            }
+//                        } else {
+//                            for (user in it.response.responseData.users) {
+//                                if (user.id != myUserId && user.id == it.message.fromUserId) {
+//                                    Glide.with(this@ChatScreenActivity)
+//                                        .load(user.avatarFileId?.let { fileId ->
+//                                            Tools.getFilePathUrl(
+//                                                fileId
+//                                            )
+//                                        })
+//                                        .placeholder(R.drawable.img_user_placeholder)
+//                                        .centerCrop()
+//                                        .into(bindingSetup.cvNotification.ivUserImage)
+//                                    val content: String =
+//                                        if (it.message.type != Const.JsonFields.TEXT_TYPE) {
+//                                            getString(
+//                                                R.string.generic_shared,
+//                                                it.message.type.toString()
+//                                                    .replaceFirstChar { type -> type.uppercase() })
+//                                        } else {
+//                                            it.message.body?.text.toString()
+//                                        }
+//
+//                                    bindingSetup.cvNotification.tvTitle.text =
+//                                        user.formattedDisplayName
+//                                    bindingSetup.cvNotification.tvMessage.text =
+//                                        content
+//                                    break
+//                                }
+//                            }
+//                        }
+//
+//
+//                        bindingSetup.cvNotification.cvRoot.visibility = View.VISIBLE
+//
+//                        val roomId = it.message.roomId
+//                        bindingSetup.cvNotification.cvRoot.setOnClickListener {
+//                            roomId?.let { roomId -> viewModel.getSingleRoomData(roomId) }
+//                        }
+//
+//                        runnable.let { runnable -> handler.removeCallbacks(runnable) }
+//
+//                        handler = Handler(Looper.getMainLooper())
+//                        Timber.d("Starting handler")
+//                        handler.postDelayed(runnable, 5000)
+//                    }
+//                }
+//
+//                Resource.Status.ERROR -> Timber.d("Failed to fetch room with users")
+//                else -> Timber.d("Other error")
+//            }
+//        })
 
         viewModel.tokenExpiredListener.observe(this, EventObserver { tokenExpired ->
             if (tokenExpired) {
