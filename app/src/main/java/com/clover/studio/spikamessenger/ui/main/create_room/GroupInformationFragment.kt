dcd6b1d5c2d68c4819ca2b2973d59601e37fe36a
MatkoTom <matko@clover.studio>
2023-08-02 12:30:13 +0200

    Small fix for unmerged file

--- app/src/main/java/com/clover/studio/spikamessenger/ui/main/create_room/GroupInformationFragment.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/ui/main/create_room/GroupInformationFragment.kt
@@ -185,7 +185,7 @@ class GroupInformationFragment : BaseFragment() {
             }
         }
 
-        binding.cvPhotoPicker.setOnClickListener {
+        binding.ivPickPhoto.setOnClickListener {
             ChooserDialog.getInstance(requireContext(),
                 getString(R.string.placeholder_title),
                 null,
