683f3db69b0d2c3d406677f5d003d0ce0557b0b9
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2023-11-24 14:50:27 +0100

    renamed const MESSAGE_IDS = "messagesIds" to "messageIds" (#361)

--- app/src/main/java/com/clover/studio/spikamessenger/utils/Const.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/utils/Const.kt
@@ -137,7 +137,7 @@ class Const {
             const val PRIVATE = "private"
             const val GROUP = "group"
             const val BODY = "body"
-            const val MESSAGE_IDS = "messagesIds"
+            const val MESSAGE_IDS = "messageIds"
             const val REACTION = "reaction"
 
             // Reply
