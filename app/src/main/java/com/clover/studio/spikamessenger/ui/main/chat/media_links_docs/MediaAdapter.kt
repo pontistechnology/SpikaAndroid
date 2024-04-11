8e291cc3130702038ce3a4b666fbad80e7780a21
ivolarevic <ivana.volarevic2@gmail.com>
2024-04-11 13:46:33 +0200

    implemented date and sticky header

--- app/src/main/java/com/clover/studio/spikamessenger/ui/main/chat/media_links_docs/MediaAdapter.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/ui/main/chat/media_links_docs/MediaAdapter.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -18,30 +18,59 @@ import com.bumptech.glide.request.target.Target
 import com.clover.studio.spikamessenger.R
 import com.clover.studio.spikamessenger.data.models.entity.Message
 import com.clover.studio.spikamessenger.databinding.MediaItemsBinding
+import com.clover.studio.spikamessenger.databinding.MonthDateBinding
 import com.clover.studio.spikamessenger.utils.Tools
+
+const val VIEW_TYPE_MEDIA_ITEM = 1
+const val VIEW_TYPE_DATE_ITEM = 2
 
 class MediaAdapter(
     private val context: Context,
     private val onItemClick: (message: Message) -> Unit
-) : ListAdapter<Message, MediaAdapter.MediaViewHolder>(MediaDiffCallback()) {
+) : ListAdapter<Message, ViewHolder>(MediaDiffCallback()) {
     inner class MediaViewHolder(val binding: MediaItemsBinding) :
-        RecyclerView.ViewHolder(binding.root)
+        ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
-        val binding =
-            MediaItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MediaViewHolder(binding)
+    inner class MonthDateViewHolder(val binding: MonthDateBinding) :
+        ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == VIEW_TYPE_MEDIA_ITEM) {
+            val binding =
+                MediaItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            MediaViewHolder(binding)
+        } else {
+            val binding =
+                MonthDateBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            MonthDateViewHolder(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
-        with(holder) {
-            getItem(position).let { message ->
+    override fun getItemViewType(position: Int): Int {
+        val message = getItem(position)
+        return if (message.roomId == null) {
+            VIEW_TYPE_DATE_ITEM
+        } else {
+            VIEW_TYPE_MEDIA_ITEM
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position).let { message ->
+            if (holder.itemViewType == VIEW_TYPE_MEDIA_ITEM) {
+                holder as MediaViewHolder
+
                 val imagePath = Tools.getMediaPath(context, message)
                 Glide.with(context)
                     .load(imagePath)
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .centerCrop()
-                    .error(AppCompatResources.getDrawable(context, R.drawable.img_media_placeholder_error))
+                    .error(
+                        AppCompatResources.getDrawable(
+                            context,
+                            R.drawable.img_media_placeholder_error
+                        )
+                    )
                     .listener(object : RequestListener<Drawable> {
                         override fun onLoadFailed(
                             e: GlideException?,
@@ -59,20 +88,23 @@ class MediaAdapter(
                             dataSource: DataSource,
                             isFirstResource: Boolean
                         ): Boolean {
-                            binding.pbMediaLoading.visibility = View.GONE
-                            binding.flLoadingScreen.visibility = View.GONE
-                            binding.ivMediaItem.visibility = View.VISIBLE
+                            holder.binding.pbMediaLoading.visibility = View.GONE
+                            holder.binding.flLoadingScreen.visibility = View.GONE
+                            holder.binding.ivMediaItem.visibility = View.VISIBLE
                             return false
                         }
                     })
-                    .into(binding.ivMediaItem)
+                    .into(holder.binding.ivMediaItem)
 
-                binding.ivMediaItem.setOnClickListener { _ ->
+                holder.binding.ivMediaItem.setOnClickListener { _ ->
                     onItemClick.invoke(message)
                 }
+
+            } else {
+                holder as MonthDateViewHolder
+                holder.binding.tvTitle.text = message.body?.text.toString()
             }
         }
-
     }
 
     private class MediaDiffCallback : DiffUtil.ItemCallback<Message>() {
--- app/src/main/java/com/clover/studio/spikamessenger/ui/main/chat/media_links_docs/MediaDetailsFragment.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/ui/main/chat/media_links_docs/MediaDetailsFragment.kt
@@ -9,24 +9,26 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.clover.studio.spikamessenger.data.models.entity.Message
+import com.clover.studio.spikamessenger.data.models.entity.MessageBody
 import com.clover.studio.spikamessenger.data.models.junction.RoomWithUsers
 import com.clover.studio.spikamessenger.databinding.FragmentMediaDetailsBinding
 import com.clover.studio.spikamessenger.ui.main.chat.ChatViewModel
 import com.clover.studio.spikamessenger.ui.main.chat.MediaScreenState
+import com.clover.studio.spikamessenger.utils.Const
 import com.clover.studio.spikamessenger.utils.extendables.BaseFragment
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.Calendar
+import java.util.Locale
 
 class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFragment() {
     private val viewModel: ChatViewModel by activityViewModels()
     private lateinit var binding: FragmentMediaDetailsBinding
 
-    private var mediaList: List<Message> = arrayListOf()
     private var mediaAdapter: MediaAdapter? = null
-    private var linearLayoutManager: LinearLayoutManager? = null
+    private var gridLayoutManager: GridLayoutManager? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,7 +51,7 @@ class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFra
     }
 
     private fun setUpAdapter() {
-        linearLayoutManager = GridLayoutManager(activity, 3)
+        gridLayoutManager = GridLayoutManager(activity, 3)
         mediaAdapter = MediaAdapter(requireContext()) {
             // On click open image
         }
@@ -58,7 +60,23 @@ class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFra
             itemAnimator = null
             isMotionEventSplittingEnabled = false
             adapter = mediaAdapter
-            layoutManager = linearLayoutManager
+            layoutManager = gridLayoutManager
+            addItemDecoration(
+                StickyHeaderDecoration(
+                    parent = this,
+                    isHeader = { position -> mediaAdapter?.getItemViewType(position) == VIEW_TYPE_DATE_ITEM }
+                )
+            )
+        }
+
+        gridLayoutManager?.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return when (mediaAdapter?.getItemViewType(position)) {
+                    VIEW_TYPE_MEDIA_ITEM -> 1
+                    VIEW_TYPE_DATE_ITEM -> 3
+                    else -> 1
+                }
+            }
         }
     }
 
@@ -67,14 +85,10 @@ class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFra
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
-                val lastVisiblePosition = linearLayoutManager?.findLastVisibleItemPosition()
-                val totalItemCount = linearLayoutManager?.itemCount
-                Timber.d("lastVisiblePosition: $lastVisiblePosition")
-                Timber.d("totalItemCount: $totalItemCount")
-
+                val lastVisiblePosition = gridLayoutManager?.findLastVisibleItemPosition()
+                val totalItemCount = gridLayoutManager?.itemCount
                 if (lastVisiblePosition != null && totalItemCount != null) {
                     if (lastVisiblePosition == totalItemCount.minus(1)) {
-                        Timber.d("Fetching next batch of data")
                         roomsWithUsers?.room?.roomId?.let { viewModel.fetchNextMediaSet(roomId = it) }
                     }
                 }
@@ -89,8 +103,17 @@ class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFra
                     when (state) {
                         is MediaScreenState.Success -> {
                             if (state.media != null) {
-                                mediaList = state.media
-                                mediaAdapter?.submitList(mediaList)
+                                val groupedMediaList = mutableListOf<Message>()
+
+                                state.media
+                                    .sortedByDescending { it.createdAt }
+                                    .groupBy { getMonthFromTimestamp(it.createdAt ?: 0) }
+                                    .forEach { (month, mediaItems) ->
+                                        groupedMediaList.add(makeDateMessage(month))
+                                        groupedMediaList.addAll(mediaItems)
+                                    }
+
+                                mediaAdapter?.submitList(groupedMediaList)
                             }
                         }
 
@@ -107,4 +130,60 @@ class MediaDetailsFragment(private val roomsWithUsers: RoomWithUsers?) : BaseFra
             }
         }
     }
+
+    private fun makeDateMessage(month: String): Message {
+        return Message(
+            id = 0,
+            fromUserId = null,
+            totalUserCount = null,
+            deliveredCount = null,
+            seenCount = null,
+            roomId = null,
+            type = Const.JsonFields.TEXT_TYPE,
+            body = MessageBody(
+                referenceMessage = null,
+                text = month,
+                thumbId = null,
+                fileId = null,
+                file = null,
+                thumb = null,
+                thumbnailData = null,
+                type = Const.JsonFields.TEXT_TYPE,
+                subject = null,
+                subjectId = null,
+                objectIds = null,
+                objects = null
+            ),
+            referenceMessage = null,
+            createdAt = null,
+            modifiedAt = null,
+            deleted = null,
+            replyId = null,
+            localId = null,
+            messageStatus = null,
+            uri = null,
+            thumbUri = null,
+            unreadCount = 0,
+            userName = "",
+            isForwarded = false
+        )
+    }
+
+    private fun getMonthFromTimestamp(timestamp: Long): String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = timestamp
+        val month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+        val year = calendar.get(Calendar.YEAR)
+        return "$month $year"
+    }
+
+    // For testing
+//    private fun getMonthAndDayFromTimestamp(timestamp: Long): String {
+//        val calendar = Calendar.getInstance()
+//        calendar.timeInMillis = timestamp
+//        val month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+//        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+//        val year = calendar.get(Calendar.YEAR)
+//        return "$month $dayOfMonth, $year"
+//    }
 }
