b42b642464e66bf681ffe3e99579618ce247a145
Matko Tomljanović <50554253+MatkoTom@users.noreply.github.com>
2024-07-11 14:26:16 +0200

    Paste Auth Code From Clipboard (#442)
    * Implemented logic to handle pasting otp codes from clipboard, if necessary.
    
    * Added a small comment
    
    * Sorted out a part of code.
    
    * Optimized sequence reader.
    
    ---------
    
    Co-authored-by: MatkoTom <matko@clover.studio>

--- app/src/main/java/com/clover/studio/spikamessenger/ui/onboarding/verification/VerificationFragment.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/ui/onboarding/verification/VerificationFragment.kt
@@ -1,6 +1,5 @@
 package com.clover.studio.spikamessenger.ui.onboarding.verification
 
-import android.content.Context
 import android.content.IntentFilter
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -99,7 +98,12 @@ class VerificationFragment : BaseFragment() {
 
     override fun onResume() {
         super.onResume()
-        ContextCompat.registerReceiver(requireContext(), smsReceiver, intentFilter, RECEIVER_EXPORTED)
+        ContextCompat.registerReceiver(
+            requireContext(),
+            smsReceiver,
+            intentFilter,
+            RECEIVER_EXPORTED
+        )
     }
 
     override fun onPause() {
@@ -215,7 +219,6 @@ class VerificationFragment : BaseFragment() {
         timer.start()
     }
 
-    // TODO maybe implement this in custom class?
     private fun setupTextWatchers() = with(binding.verificationInputFields) {
         //GenericTextWatcher here works only for moving to next EditText when a number is entered
         //first parameter is the current EditText and second parameter is next EditText
@@ -440,11 +443,20 @@ class VerificationFragment : BaseFragment() {
         }
 
         override fun onTextChanged(
-            arg0: CharSequence,
+            sequence: CharSequence,
             arg1: Int,
             arg2: Int,
             arg3: Int
-        ) { // TODO Auto-generated method stub
+        ) = with(binding.verificationInputFields) {
+            // This logic handles copying codes from clipboard and pasting them into the code field.
+            if (sequence.length > 1) {
+                etInputOne.setText(sequence[0].toString())
+                etInputTwo.setText(sequence[1].toString())
+                etInputThree.setText(sequence[2].toString())
+                etInputFour.setText(sequence[3].toString())
+                etInputFive.setText(sequence[4].toString())
+                etInputSix.setText(sequence[5].toString())
+            }
         }
     }
 
