3ce0ac5eb10e6f4380388afc4e859aab6b579da9
ivolarevic <ivana.volarevic2@gmail.com>
2024-08-08 15:39:36 +0200

    rebase fix

--- app/src/main/java/com/clover/studio/spikamessenger/utils/Tools.kt
+++ app/src/main/java/com/clover/studio/spikamessenger/utils/Tools.kt
@@ -794,7 +794,7 @@ object Tools {
     fun setTheme(theme: String): Int {
         return when (theme) {
             Const.Themes.MINT_THEME -> {
-                // R.style.Theme_App_LightGreen
+                //R.style.Theme_App_LightGreen
                 R.style.Theme_App_QATheme
             }
 
